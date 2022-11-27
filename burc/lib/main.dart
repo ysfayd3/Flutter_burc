92f069e4a3cf6a0561b06ddef7a1f385b3208e5a
Yusuf <73791364+YUSUFAYDIN14@users.noreply.github.com>
2022-11-27 14:31:30 +0300

    ilk flutter commit

---
+++ burc/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:burc/burc_Listesi.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.pink
+      ),
+      home: BurcListesi(),
+    );
+  }
+}
+
